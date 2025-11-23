1d3bf13e4e74475e8038dd843b122bb1fb91bb74
jainil <jainilgondaliya63@gmail.com>
2025-11-23 16:34:05 +0530

    feat: how can i help you and give silly response

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def main():
         say_jarvis("Oh, I feel happy by hearing that!")
     elif user_input.lower() in ["im not fine", "i feel bad", "im sick"]:
         say_jarvis("Oh, I feel bad by hearing that!")
-    else :
+    else:
         say_jarvis("Sorry, I didnt understand that!")
-
+    say_jarvis("How can I help you?")
+    take_user_input()
+    say_jarvis("I didnt understand that!")
     say_jarvis("Good bye, take care!")
+
 
 if __name__ == "__main__":
     main()
