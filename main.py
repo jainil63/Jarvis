84d75071ee124546a233b8b6185c50d817227425
jainil <jainilgondaliya63@gmail.com>
2025-11-23 12:26:17 +0530

    feat: jarvis act based as user input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def take_user_input():
-    input("User: ")
+    x = input("User: ")
     print()
+    return x
 
 def say_jarvis(message):
     print(f"Jarvis: {message}")  
@@ -9,8 +10,12 @@ def say_jarvis(message):
 def main(): 
     say_jarvis("Hello")
     say_jarvis("I'm currently busy! can we talk later?")
-    take_user_input()
-    say_jarvis("Bye, Thank you!")
+
+    user_input = take_user_input()
+    if user_input == "yes":
+        say_jarvis("Bye, Thank you!")
+    else:
+        say_jarvis("Nope, It's final, I have to go.")
 
 if __name__ == "__main__":
     main()
