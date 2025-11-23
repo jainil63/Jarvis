acb3bc3cef4ceeed5cad5cd425c24317796720ca
jainil <jainilgondaliya63@gmail.com>
2025-11-23 11:49:12 +0530

    take user input

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+def say_user():
+    input("User: ")
+
 def say_jarvis(message):
     print(f"Jarvis: {message}")  
 
 def main(): 
     say_jarvis("Hello\n")
-    say_jarvis("I'm currently busy!\n")
-    say_jarvis("Bye")
+    say_jarvis("I'm currently busy! can we talk later?\n")
+    say_user()
+    print()
+    say_jarvis("Bye, Thank you!")
 
 if __name__ == "__main__":
     main()
