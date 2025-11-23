95fa8f93df769a30c4da17500ee36bf00b2e46fd
jainil <jainilgondaliya63@gmail.com>
2025-11-23 12:52:30 +0530

    feat: recognize and compare different response

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     say_jarvis("I'm currently busy! can we talk later?")
 
     user_input = take_user_input()
-    if user_input == "yes":
+    if user_input.lower() in ["yes", "no problem", "np", "ok"]:
         say_jarvis("Bye, Thank you!")
     else:
         say_jarvis("Nope, It's final, I have to go.")
