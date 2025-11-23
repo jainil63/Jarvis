5c785d497d6cb310aae51d5636daa2d50de34a0f
jainil <jainilgondaliya63@gmail.com>
2025-11-23 11:20:53 +0530

    rude jarvis response

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hii from jarvis!")
-    print("Bye from jarvis!")
+def say_jarvis(message):
+    print(f"Jarvis: {message}")  
+
+def main(): 
+    say_jarvis("Hello\n")
+    say_jarvis("I'm currently busy!\n")
+    say_jarvis("Bye")
 
 if __name__ == "__main__":
     main()
