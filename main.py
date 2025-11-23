181f05d16442ad7bfe6ec5bf567fa67c7442552a
jainil <jainilgondaliya63@gmail.com>
2025-11-23 16:25:07 +0530

    feat: how are you

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from utils import take_user_input, say_jarvis
 
 
 def main():
-    say_jarvis("Hello")
-    say_jarvis("I'm currently busy! can we talk later?")
+    say_jarvis("Hello, How are you?")
 
     user_input = take_user_input()
-    if user_input.lower() in ["yes", "no problem", "np", "ok"]:
-        say_jarvis("Bye, Thank you!")
-    else:
-        say_jarvis("Nope, It's final, I have to go.")
+    if user_input.lower() in ["im fine", "all right", "nice", "fine"]:
+        say_jarvis("Oh, I feel happy by hearing that!")
+    elif user_input.lower() in ["im not fine", "i feel bad", "im sick"]:
+        say_jarvis("Oh, I feel bad by hearing that!")
+    else :
+        say_jarvis("Sorry, I didnt understand that!")
 
+    say_jarvis("Good bye, take care!")
 
 if __name__ == "__main__":
     main()
