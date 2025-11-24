7859e0ea00621e63b93be75315469173ec4ee120
jainil <jainilgondaliya63@gmail.com>
2025-11-24 17:47:04 +0530

    feat: showing camera

--- main.py
+++ main.py
@@ -1,36 +1,50 @@
 from utils import take_user_input, say_jarvis
 import os
-import cv2
+
+
+def show_camera():
+    say_jarvis("Showing camera!!")
 
 
 def main():
     say_jarvis("Hello, How are you?")
 
     user_input = take_user_input()
+
     if user_input.lower() in ["im fine", "all right", "nice", "fine"]:
         say_jarvis("Oh, I feel happy by hearing that!")
+
     elif user_input.lower() in ["im not fine", "i feel bad", "im sick"]:
         say_jarvis("Oh, I feel bad by hearing that!")
+
     else:
         say_jarvis("Sorry, I didnt understand that!")
 
     say_jarvis("How can I help you?")
+
     user_input = take_user_input()
+
     if user_input.lower() in ["shutdown device", "shutdown pc"]:
         os.system("echo shutdown /s /t 0")
         return None
+
     elif user_input.lower() in ["restart", "restart pc"]:
         os.system("echo shutdown /r /t 0")
         return None
+
     elif user_input.lower() in ["bye", "good bye"]:
         say_jarvis("Good bye sir.")
         quit(0)
+
     elif user_input.lower() in ["open vscode"]:
         os.system("code")
+
     elif user_input.lower() in ["open chrome"]:
         os.startfile("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
+
     elif user_input.lower() in ["open camera"]:
-        say_jarvis("Sorry, I can't access camera!")
+        show_camera()
+
     else:
         say_jarvis("I didn't understand")
 
